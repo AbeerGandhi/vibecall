e37126a464ae79092c86c525832afcd43fbadde8
Abeer Gandhi <abeergandhi2@gmail.com>
2024-08-15 18:57:01 +0530

    Some changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,26 @@
+'use client';
+import { useState, useEffect } from 'react';
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-  const now = new Date();
+  const [currentTime, setCurrentTime] = useState({
+    time: '',
+    date: '',
+  });
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+      const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(now);
+      setCurrentTime({ time, date });
+    };
+
+    updateTime(); // Set the initial time and date
+    const intervalId = setInterval(updateTime, 1000); // Update every second
+
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, []);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
@@ -14,8 +30,8 @@ const Home = () => {
             Schedule your next meeting!
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{currentTime.time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{currentTime.date}</p>
           </div>
         </div>
       </div>
